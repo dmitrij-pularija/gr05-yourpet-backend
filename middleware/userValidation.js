5f5dfacb7290d96ec584dcb7855cdf457e01c3b7
YuriySamusew <6177385@gmail.com>
2023-05-13 14:12:15 +0300

    registration, login, logout (without verification), joi validation according to technikal task

--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -2,86 +2,92 @@ const Joi = require("joi");
 const { unlinkSync } = require("fs");
 const getError = require("../utilities/validationError");
 
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const passRegexp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{6,16}$/;
+
 const schema = {
-  register: Joi.object({
-    name: Joi.string().min(3).max(20),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).max(20).required(),
-    subscription: Joi.string().valid("starter", "pro", "business"),
-  }).unknown(false),
-  verify: Joi.object({
-    email: Joi.string().email().required(),
-  }).unknown(false),
-  login: Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).max(20).required(),
-  }).unknown(false),
-  subscription: Joi.object({
-    subscription: Joi.string().valid("starter", "pro", "business").required(),
-  }).unknown(false),
-  avatar: Joi.object({
-    fieldname: Joi.string().valid("avatar").required(),
-    mimetype: Joi.string()
-      .valid("image/jpeg", "image/png", "image/jpeg", "image/gif")
-      .required()
-      .messages({ "any.only": "The file format must be jpg or png" }),
-    size: Joi.number()
-      .max(1 * 1024 * 1024)
-      .required()
-      .messages({ "number.max": "The file size must not exceed 1 MB" }),
-  })
-    .required()
-    .unknown(true),
+	register: Joi.object({
+		name: Joi.string().min(3).max(20),
+		email: Joi.string().email().pattern(emailRegexp).required(),
+		password: Joi.string().pattern(passRegexp).required(),
+		subscription: Joi.string().valid("starter", "pro", "business"),
+	}).unknown(false),
+	verify: Joi.object({
+		email: Joi.string().email().pattern(emailRegexp).required(),
+	}).unknown(false),
+	login: Joi.object({
+		email: Joi.string().email().pattern(emailRegexp).required(),
+		password: Joi.string().pattern(passRegexp).required(),
+	}).unknown(false),
+	subscription: Joi.object({
+		subscription: Joi.string()
+			.valid("starter", "pro", "business")
+			.required(),
+	}).unknown(false),
+	avatar: Joi.object({
+		fieldname: Joi.string().valid("avatar").required(),
+		mimetype: Joi.string()
+			.valid("image/jpeg", "image/png", "image/jpeg", "image/gif")
+			.required()
+			.messages({ "any.only": "The file format must be jpg or png" }),
+		size: Joi.number()
+			.max(1 * 1024 * 1024)
+			.required()
+			.messages({ "number.max": "The file size must not exceed 1 MB" }),
+	})
+		.required()
+		.unknown(true),
 };
 
 const registerValidation = ({ body }, res, next) => {
-  const { error } = schema.register.validate(body);
+	const { error } = schema.register.validate(body);
 
-  if (error)
-    return res.status(400).json({ message: getError(error, "register") });
+	if (error)
+		return res.status(400).json({ message: getError(error, "register") });
 
-  next();
+	next();
 };
 
 const verifyValidation = ({ body }, res, next) => {
-  const { error } = schema.verify.validate(body);
+	const { error } = schema.verify.validate(body);
 
-  if (error)
-    return res.status(400).json({ message: getError(error, "verify") });
+	if (error)
+		return res.status(400).json({ message: getError(error, "verify") });
 
-  next();
+	next();
 };
 
 const loginValidation = ({ body }, res, next) => {
-  const { error } = schema.login.validate(body);
+	const { error } = schema.login.validate(body);
 
-  if (error) return res.status(400).json({ message: getError(error, "login") });
+	if (error)
+		return res.status(400).json({ message: getError(error, "login") });
 
-  next();
+	next();
 };
 
 const subscriptionValidation = ({ body }, res, next) => {
-  const { error } = schema.subscription.validate(body);
+	const { error } = schema.subscription.validate(body);
 
-  if (error)
-    return res.status(400).json({ message: getError(error, "register") });
+	if (error)
+		return res.status(400).json({ message: getError(error, "register") });
 
-  next();
+	next();
 };
 
 const avatarValidation = ({ file }, res, next) => {
-  const { error } = schema.avatar.validate(file);
-  if (error) {
-    unlinkSync(file.path);
-    return res.status(400).json({ message: getError(error, "avatar") });
-  }
-  next();
+	const { error } = schema.avatar.validate(file);
+	if (error) {
+		unlinkSync(file.path);
+		return res.status(400).json({ message: getError(error, "avatar") });
+	}
+	next();
 };
 
 module.exports = {
-  registerValidation,
-  verifyValidation,
-  loginValidation,
-  subscriptionValidation,
-  avatarValidation,
+	registerValidation,
+	verifyValidation,
+	loginValidation,
+	subscriptionValidation,
+	avatarValidation,
 };
--- models/user/schemas.js
+++ models/user/schemas.js
@@ -1,55 +1,55 @@
 const { Schema, model } = require("mongoose");
 
 const user = new Schema(
-  {
-    name: {
-      type: String,
-      minlength: 3,
-      maxlength: 20,
-    },
-    password: {
-      type: String,
-      minlength: 6,
-      required: [true, "Password is required"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-    },
-    subscription: {
-      type: String,
-      enum: ["starter", "pro", "business"],
-      default: "starter",
-    },
-    avatarURL: {
-      type: String,
-    },
-    token: {
-      type: String,
-      default: null,
-    },
-    verify: {
-      type: Boolean,
-      default: false,
-    },  
-    verificationToken: {
-      type: String,
-      default: null,
-      required: [true, "Verify token is required"],
-    },
-  },
-  { versionKey: false, timestamps: false }
+	{
+		name: {
+			type: String,
+			minlength: 3,
+			maxlength: 20,
+		},
+		password: {
+			type: String,
+			minlength: 6,
+			required: [true, "Password is required"],
+		},
+		email: {
+			type: String,
+			required: [true, "Email is required"],
+			unique: true,
+		},
+		subscription: {
+			type: String,
+			enum: ["starter", "pro", "business"],
+			default: "starter",
+		},
+		avatarURL: {
+			type: String,
+		},
+		token: {
+			type: String,
+			default: null,
+		},
+		verify: {
+			type: Boolean,
+			default: false,
+		},
+		verificationToken: {
+			type: String,
+			default: null,
+			required: [true, "Verify token is required"],
+		},
+	},
+	{ versionKey: false, timestamps: false }
 );
 
 user.post("save", (error, data, next) => {
-  const field = Object.keys(error.keyPattern)[0];
-  const message = field === "email" ? "Email in use" : error.message;
-  const responseError = {
-    status: 409,
-    message: message,
-  };
-  return next(responseError);
+	const field = Object.keys(error.keyPattern)[0];
+	const message = field === "email" ? "Email in use" : error.message;
+	const responseError = {
+		status: 409,
+		message: message,
+	};
+	return next(responseError);
 });
 
 const User = model("user", user);
