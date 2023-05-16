825cd912ed6f47381aab05f33af01f668394c091
vitsemashko <107743238+vitsemashko@users.noreply.github.com>
2023-05-16 11:54:20 +0300

    add logic for get all news and news by title

--- models/news/schemas.js
+++ models/news/schemas.js
@@ -1,40 +1,28 @@
 const { Schema, model } = require("mongoose");
 
-const contacts = new Schema(
-  {
-    name: {
-      type: String,
-      minlength: 3,
-      maxlength: 20,
-      required: [true, "Set name for contact"],
-    },
-    email: {
-      type: String,
-      required: [true, "Set email for contact"],
-    },
-    phone: {
-      type: String,
-      minlength: 6,
-      maxlength: 20,
-      required: [true, "Set phone for contact"],
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-  },
-  { versionKey: false, timestamps: false }
+const news = new Schema(
+	{
+		title: {
+			type: String,
+			required: [true, "Set title"],
+		},
+		url: {
+			type: String,
+		},
+		description: {
+			type: String,
+		},
+		date: {
+			type: String,
+			default: null,
+		},
+	},
+	{
+		versionKey: false,
+		timestamps: false,
+	}
 );
 
-contacts.post("save", (error, data, next) => {
-  error.status = 400;
-  next();
-});
+const NewsModel = model("news", news);
 
-const Contacts = model("contacts", contacts);
-
-module.exports = Contacts;
+module.exports = NewsModel;
