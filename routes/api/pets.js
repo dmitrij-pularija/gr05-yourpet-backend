790ba29062275904c84008bbadbd731e100e4798
Rodion Myastkowskuy <rodion.myastkowskuy@gmail.com>
2023-05-17 21:19:19 +0300

    add image pets

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -4,22 +4,33 @@ const router = express.Router();
 // const ctrlWrapper = require("../../decorators/ctrl");
 const ctrl = require("../../decorators/ctrlWrap");
 const authenticate = require("../../middleware/authenticate");
-const { listPets, add, del } = require("../../controllers/pets.js");
+const {
+  listPets,
+  add,
+  del,
+  addImagePets,
+  delImage,
+} = require("../../controllers/pets.js");
 const uploads = require("../../middleware/upload");
 
 const {
-	addValidation,
-	isValidId,
+  addValidation,
+  isValidId,
 } = require("../../middleware/petsValidation.js");
 
 router.get("/", authenticate, listPets);
-router.post(
-	"/",
-	authenticate,
-	addValidation,
-	uploads.pets.single("image"),
-	ctrl(add)
+
+router.post("/", authenticate, addValidation, ctrl(add));
+
+router.patch(
+  "/add",
+  authenticate,
+  uploads.pets.single("pet"),
+  ctrl(addImagePets)
 );
+
+router.delete("/delete-photo/:id", authenticate, ctrl(delImage));
+
 router.delete("/:id", authenticate, isValidId, ctrl(del));
 
 module.exports = router;
